8b194b41b3b044ac7f34447c0bfb74c1f74fb9b3
Marcin Milewski <mmilewski@gmail.com>
2010-02-26 14:24:24 +0000

    - dodałem poruszanie się buźką po ekranie (automatyczne) - zmieniłem wcięcia z tabów na spacje

--- LandChoiceScreen.cpp
+++ LandChoiceScreen.cpp
@@ -1,5 +1,6 @@
 #include <SDL/SDL_opengl.h>
-#include <boost/assign/std/vector.hpp> // for 'operator+=()'
+#include <boost/assign/std/vector.hpp>  // 'operator+=()'
+#include <cmath>  // fabs
 
 #include "Engine.h"
 #include "LandChoiceScreen.h"
@@ -7,47 +8,50 @@
 
 
 LandChoiceScreen::LandChoiceScreen() :
-	AppState(),
-	face_pos_x(.1),
-	face_pos_y(.5)
-	{
+    face_pos_x(0),
+    face_pos_y(0),
+    from_node(0),
+    to_node(0)
+    {
 
 //
 //	0 -------- 1
 //	           |
 //	           |
-//	  3 ------ 2
+//     3 ----- 2
 //	           |
 //	           |
 //	           4
 
-//	float positions[][2] = {
-//			{ .1, .1 },
-//			{ .6, .1 },
-//			{ .6, .4 },
-//			{ .2, .4 },
-//			{ .6, .7 },
-//	};
-
 	using namespace boost::assign;
-	typedef std::vector<int> IntVector;
-	std::vector< IntVector > connections;
-	connections += IntVector(), IntVector(), IntVector(), IntVector(), IntVector();
-	connections.at(0) += 1;
-	connections.at(1) += 0, 2;
-	connections.at(2) += 1, 3, 4;
-	connections.at(3) += 2;
-	connections.at(4) += 2;
+    connections += IntVector(), IntVector(), IntVector(), IntVector(), IntVector();
+    connections.at(0) += 1;
+    connections.at(1) += 2, 0;
+    connections.at(2) += 3, 4, 1;
+    connections.at(3) += 2;
+    connections.at(4) += 2;
 
-	typedef std::vector<bool> BoolVector;
-	const int Up=1, Left=2, Down=3, Right=4;
-	std::vector< BoolVector > moves;
-	moves += BoolVector(), BoolVector(), BoolVector(), BoolVector(), BoolVector();
-	moves.at(0) += Right;
-	moves.at(1) += Down, Left;
-	moves.at(2) += Up, Left, Down;
-	moves.at(3) += Right;
-	moves.at(4) += Up;
+//
+//    http://en.wikipedia.org/wiki/C%2B%2B0x#Initializer_lists
+//    std::vector<int> hello = {1,2,3,4};
+//
+
+    const int Up = 1, Left = 2, Down = 3, Right = 4;
+    moves += BoolVector(), BoolVector(), BoolVector(), BoolVector(), BoolVector();
+    moves.at(0) += Right;
+    moves.at(1) += Down, Left;
+    moves.at(2) += Up, Left, Down;
+    moves.at(3) += Right;
+    moves.at(4) += Up;
+
+    m_positions += Point( .1, .7 ), Point( .7, .7 ), Point( .7, .4 ), Point( .3, .4 ), Point( .7, .1 );
+
+    face_pos_x = m_positions.at(0)[0];
+    face_pos_y = m_positions.at(0)[1];
+
+    const SpriteConfigData land_choice_sprite_data(DL::Foreground, 8, 0, 0, 14 * 32, 32, 32, false);
+    m_sprite.reset(new Sprite(land_choice_sprite_data));
+
 }
 
 LandChoiceScreen::~LandChoiceScreen() {
@@ -64,59 +68,107 @@ void LandChoiceScreen::Start() {
 #include "SpriteConfig.h"
 
 void LandChoiceScreen::Draw() {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glLoadIdentity();
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
 //	Engine::Get().Renderer()->DrawQuad(.1, .1, .9, .9, 1, 0, 0, 1);
 
-	const SpriteConfigData land_choice_sprite_data(DL::Foreground, 6, 0, 0, 14*32, 32, 32, false);
 
-	const float point_width = .1, point_height = .1;
-	const int points_count = 5;
-	float positions[points_count][2] = {
-			{ .2, .7 },
-			{ .7, .7 },
-			{ .7, .4 },
-			{ .3, .4 },
-			{ .7, .1 },
-	};
+    const float point_width = 1.0/10, point_height = 1.0/10;
+//    const int points_count = 5;
+//    const Point positions[points_count] = {
+//            Point( .1, .7 ),
+//            Point( .7, .7 ),
+//            Point( .7, .4 ),
+//            Point( .3, .4 ),
+//            Point( .7, .1 ),
+//    };
 
-	const Sprite sprite(land_choice_sprite_data);
-	for (int i = 0; i < points_count; ++i) {
-		sprite.SetCurrentFrame(i);
-		const float x = positions[i][0] - point_width/2;
-		const float y = positions[i][1] - point_height/2;
-		sprite.DrawCurrentFrame(x, y, point_width, point_height);
-//		Engine::Get().Renderer()->DrawSprite(
-//				i*32, 14*32, 32, 32,
-//				x, y, point_width, point_height,
-//				DL::Foreground );
-	}
+    // narysuj drogę
 
-	SDL_GL_SwapBuffers();
+    // TOOD: implement
+    //  ....
+
+    // narysuj węzły
+    for (size_t i = 0; i < m_positions.size(); ++i) {
+        m_sprite->SetCurrentFrame(i);
+        const float x = m_positions[i][0] - point_width / 2;
+        const float y = m_positions[i][1] - point_height / 2;
+        m_sprite->DrawCurrentFrame(x, y, point_width, point_height);
+//        Engine::Get().Renderer()->DrawSprite(
+//                i * 32, 14 * 32, 32, 32,
+//                x, y,
+//                point_width, point_height, DL::Foreground);
+    }
+
+    // narysuj postać
+    m_sprite->SetCurrentFrame(8);
+    m_sprite->DrawCurrentFrame(face_pos_x-point_width/2, face_pos_y-point_height/2, point_width, point_height);
+
+    SDL_GL_SwapBuffers();
 }
 
+// zwraca znak x
+int sgn(float x) {
+    if (x < 0)  return -1;
+    if (x > 0)  return 1;
+    return 0;
+}
+
+// następny węzeł (dowolny, z którym jest połączenie). Lub node, jeżeli nie ma połączeń
+int LandChoiceScreen::next_node(int node) const {
+    if (false == connections.at(node).empty()) {
+        return connections.at(node).at(rand()%connections.at(node).size());
+    }
+    return node;
+}
+
+#include <iostream>
+
 bool LandChoiceScreen::Update(double dt) {
-	return !IsDone();
+    if (from_node == to_node) {
+        to_node = next_node(to_node);
+    }
+    assert(from_node!=to_node && "from == to :/");
+
+    const Point from_node_pos = m_positions.at(from_node);
+    const Point to_node_pos = m_positions.at(to_node);
+
+    float dist_x = to_node_pos.x - face_pos_x;
+    float dist_y = to_node_pos.y - face_pos_y;
+    float vel_x = .3 * sgn(dist_x);
+    float vel_y = .2 * sgn(dist_y);
+
+    if(fabs(dist_x)<.01 && fabs(dist_y)<.01) {
+        from_node = to_node;
+        vel_x = vel_y = 0;
+        face_pos_x = to_node_pos.x;
+        face_pos_y = to_node_pos.y;
+    }
+
+    face_pos_x += vel_x * dt;
+    face_pos_y += vel_y * dt;
+
+    return !IsDone();
 }
 
 void LandChoiceScreen::ProcessEvents(const SDL_Event & event) {
-	if (event.type == SDL_QUIT) {
-		SetDone();
-	}
+    if (event.type == SDL_QUIT) {
+        SetDone();
+    }
 
-	if (event.type == SDL_KEYDOWN) {
-		SDLKey key = event.key.keysym.sym;
-		if (key == SDLK_ESCAPE) {
-			SetDone();
-		}
-	}
+    if (event.type == SDL_KEYDOWN) {
+        SDLKey key = event.key.keysym.sym;
+        if (key == SDLK_ESCAPE) {
+            SetDone();
+        }
+    }
 }
 
 boost::shared_ptr<AppState> LandChoiceScreen::NextAppState() const {
-	//
-	// TODO: zaimplementować zwracanie następnego stanu
-	//
+    //
+    // TODO: zaimplementować zwracanie następnego stanu
+    //
 
-	return boost::shared_ptr<AppState>(new MainMenu);
+    return boost::shared_ptr<AppState>(new MainMenu);
 }
 
--- LandChoiceScreen.h
+++ LandChoiceScreen.h
@@ -2,11 +2,12 @@
 #define __LANDCHOICESCREEN_H_INCLUDED__
 
 #include "AppState.h"
+#include "Sprite.h"
 
 class LandChoiceScreen: public AppState {
 public:
-	LandChoiceScreen();
-	virtual ~LandChoiceScreen();
+    LandChoiceScreen();
+    virtual ~LandChoiceScreen();
     void Init();
     void Start();
 
@@ -17,15 +18,33 @@ public:
     boost::shared_ptr<AppState> NextAppState() const;
 
 private:
-    struct FS {
-    	enum FaceState {
-    		WALK,
-    		STAND,
-    	};
-    };
-    float face_pos_x;      // położenie bohatera na ekranie
-    float face_pos_y;
+//    struct FS {
+//        enum FaceState {
+//            WALK, STAND,
+//        };
+//    };
+    int next_node(int node) const;
 
+    struct Point {
+        Point(float x, float y) : x(x), y(y) {}
+        float operator[](int idx) const { return (idx==0?x:y); }
+        float& operator[](int idx) { return (idx==0?x:y); }
+        float x, y;
+    };
+
+    typedef std::vector<bool> BoolVector;
+    typedef std::vector<int> IntVector;
+
+    std::vector<BoolVector> moves;
+    std::vector<IntVector> connections;
+    std::vector<Point> m_positions;     // pozycje punktów
+
+    float face_pos_x;       // położenie bohatera na ekranie
+    float face_pos_y;
+    int from_node;          // numer węzła początkowego
+    int to_node;            // numer węzła docelowego
+
+    SpritePtr m_sprite;
 };
 
 #endif
