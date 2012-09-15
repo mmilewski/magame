a4937630a7dcc3c370def876e9ec341fecf17bd9
Marcin Milewski <mmilewski@gmail.com>
2012-09-15 13:03:37 +0200

    updates #10 - Refactor ScrollBox::Draw (computations cleanup, add comments)

--- gui/ScrollBox.cpp
+++ gui/ScrollBox.cpp
@@ -18,22 +18,41 @@ void ScrollBox::Draw() const
     if (m_items.empty()) {
         return;
     }
-    const int display_before = 2, display_after = 2;
-    const double itemHeight = 0.07;
     RendererPtr renderer = Engine::Get().GetRenderer();
 
+    // config values
+    const int display_before_selected = 2, display_after_selected = 3;
+    const double item_height = 0.07, font_height = 0.05;
+
+    // caption
     Text(0.07, 0.1).DrawText("Select level", 0.05, 0.8);
-    Text text(0.05, 0.05);
-    double sel_y = (GetSizedPosition().Y() - GetPosition().Y())/2 + GetPosition().Y();
-    const Position highlight = Position(GetPosition().X(), sel_y-0.02);
-    renderer->DrawQuad(highlight, highlight+Size(GetSize().X(),itemHeight+0.02), Rgba(0,1,0, .5));
-    text.DrawText(m_items.at(SelectedId()), GetPosition().X(), sel_y);
+
+    // few useful values
+    const Text text(0.05, font_height);
     const int sel_id = static_cast<int>(SelectedId());
-    for (int idx = sel_id - 1; (idx >= 0) && (idx+display_before >= sel_id); --idx) {
-        text.DrawText(m_items.at(idx), GetPosition().X(), sel_y + itemHeight * (sel_id-idx));
+    const double sel_y = (GetPosition() + GetSize()/2).Y();  // selection is widget-centered
+    const double centered_sel_y = sel_y + (item_height-font_height)/2; // text is selection-centered
+
+    // draw highlight (background for selected item)
+    const Position highlight_pos = Position(GetPosition().X(), sel_y-0.01);
+    const Size highlight_size = Size(GetSize().X(), item_height+0.02);
+    renderer->DrawQuad(highlight_pos, highlight_pos + highlight_size, Rgba(0,1,0, .5));
+
+    // draw selected item
+    text.DrawText(m_items.at(SelectedId()), GetPosition().X(), centered_sel_y);
+
+    // draw items before selected
+    for (int idx = sel_id - 1; (idx >= 0) && (idx+display_before_selected >= sel_id); --idx) {
+        text.DrawText(m_items.at(idx),
+                      GetPosition().X(),
+                      centered_sel_y + item_height * (sel_id-idx));
     }
-    for (int idx = sel_id + 1; (idx < (int)m_items.size()) && (idx-sel_id <= display_after); ++idx) {
-        text.DrawText(m_items.at(idx), GetPosition().X(), sel_y - itemHeight * (idx-sel_id));
+
+    // draw items after selected
+    for (int idx = sel_id + 1; (idx < (int)m_items.size()) && (idx-sel_id <= display_after_selected); ++idx) {
+        text.DrawText(m_items.at(idx),
+                      GetPosition().X(),
+                      centered_sel_y - item_height * (idx-sel_id));
     }
 }
 
