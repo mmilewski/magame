cf28c68a7e795caf8bbf7389b443f65fd00dce88
Marcin Milewski <mmilewski@gmail.com>
2013-03-31 01:28:30 -0400

    Remove debug prints to cerr

--- editor/SetFieldCommand.cpp
+++ editor/SetFieldCommand.cpp
@@ -4,7 +4,6 @@
 #include "EditorCommand.h"
 
 void SetFieldCommand::Execute(Editor* editor) {
-    std::cerr << "Executing SetFieldCommand\n";
     m_saved_field = editor->GetFieldAt(m_pos);
     editor->SetFieldAt(m_pos, m_field);
 }
@@ -14,7 +13,6 @@ void SetFieldCommand::Undo(Editor* editor) {
 }
 
 bool SetFieldCommand::IsReady() const {
-    std::cerr << "SetFieldCommand::IsReady " << m_pos << " " << m_field << "\n";
     if (m_pos.X() < 0 || m_pos.Y() < 0) return false;
     if (m_field == FT::COUNT) return false;
     return true;
